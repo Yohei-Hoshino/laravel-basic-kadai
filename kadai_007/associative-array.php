91d98d01a9a7fc21128ef7dbdd2b83b71af55057
hoshinoyohei <personalyohei@gmail.com>
2024-07-29 20:56:42 +0900

    不要ファイルを削除しました

--- kadai_007/associative-array.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$food_data = ['name' => 'onion', 'price' => 200, 'weight' => 160];
-
-print_r($food_data);
-
-?>
--- kadai_011/foreach.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$food_data = ['名前' => '玉ねぎ', '値段' => 200, '産地' => '北海道'];
-
-foreach($food_data as $key => $value) {
-  echo "{$key}:{$value}<br>";
-}
-
-?>
